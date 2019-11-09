4d6f4e8b54898a9dac0e0a82446250ab67712678
Dreamer <christopherlu.pdx@gmail.com>
2019-11-09 14:48:09 -0800

    added select char

--- drawBig.cpp
+++ drawBig.cpp
@@ -11,32 +11,32 @@ using namespace std;
 //not sure what params drawRow should i/o
 void drawRow(int a);
 int requestNumb(int& int1);
+char requestChar(char& char1);
 
 int main()
 {
 	int int1;
-
+	char char1;
 	cout << "this is what it will do" << endl;
 
 	//requests an integer
 	requestNumb(int1);
-	
+	requestChar(char1);
 	drawRow(int1);
 	
 
 	return 0;
 }
 
+
+//draw a line of the given integer size
 void drawRow(int a){
 string shape;
 int i;
-//draw a line of the given integer size
 for (i=0; i<a; i++)
 {
 	cout <<"*";
 }
-
-
 }
 
 //the user request prompt is in a funciton rejects negatives, 
@@ -48,4 +48,11 @@ do {
 	cin >> int1;
    }
 while(int1 < 0);
+}
+
+//Requests character to draw the X with
+char requestChar(char& char1)
+{
+	cout << "gimme a char" << endl;
+	cin >> char1;
 }
